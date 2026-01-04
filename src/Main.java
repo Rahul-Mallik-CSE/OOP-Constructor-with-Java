4e7f44c337f7a50bbf80b73d9dc5cf90bcce0ec2
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-04 10:26:27 +0600

    Add Student class with default constructor and update Main class to create a Student instance

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@
 class Student{
     String name;
     int age;
+    // If we don't write any constructor, Java provides a default No-Argument Constructor. That will be shown below: 
+    public Student(){
+        //empty body for default constructor
+    }
 }
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        Student s1 = new Student();
     }
 }
