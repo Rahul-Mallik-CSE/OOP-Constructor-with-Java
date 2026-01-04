40e35f739376f0daa74ef64a66350f2cbfa7ec5d
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-04 10:31:00 +0600

    Enhance Student class with parameterized constructor and update Main class to demonstrate usage

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,28 @@ class Student{
     public Student(){
         //empty body for default constructor
     }
+
+    //Parameterized Constructor
+    public Student(String n, int a){
+        name= n;
+        age= a;
+    }
 }
 
 public class Main {
     public static void main(String[] args){
+        
+    // Using Default Constructor
         Student s1 = new Student();
+        System.out.println("Default Constructor:");
+        System.out.println("Name: " + s1.name); // null
+        System.out.println("Age: " + s1.age);   // 0
+
+        
+        // Using Parameterized Constructor
+        Student s2 = new Student("Alice", 20);
+        System.out.println("Parameterized Constructor:");
+        System.out.println("Name: " + s2.name); // Alice
+        System.out.println("Age: " + s2.age);   // 20
     }
 }
